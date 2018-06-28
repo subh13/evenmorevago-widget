603a15e8d42cccb52ae3a272cb44f3e7840c7602
Tier511 <work@tier5.us>
2018-06-28 19:29:53 +0530

    pop up ready watch for change in myIndex baseline v0.1

--- app.js
+++ app.js
@@ -10955,7 +10955,6 @@
 	 */
     e.exports=function(e){return null!=e&&(n(e)||r(e)||!!e._isBuffer)}},function(e,t,n){"use strict";function r(e){this.defaults=e,this.interceptors={request:new s,response:new s}}var o=n(6),i=n(2),s=n(17),u=n(18);r.prototype.request=function(e){"string"==typeof e&&(e=i.merge({url:arguments[0]},arguments[1])),e=i.merge(o,{method:"get"},this.defaults,e),e.method=e.method.toLowerCase();var t=[u,void 0],n=Promise.resolve(e);for(this.interceptors.request.forEach(function(e){t.unshift(e.fulfilled,e.rejected)}),this.interceptors.response.forEach(function(e){t.push(e.fulfilled,e.rejected)});t.length;)n=n.then(t.shift(),t.shift());return n},i.forEach(["delete","get","head","options"],function(e){r.prototype[e]=function(t,n){return this.request(i.merge(n||{},{method:e,url:t}))}}),i.forEach(["post","put","patch"],function(e){r.prototype[e]=function(t,n,r){return this.request(i.merge(r||{},{method:e,url:t,data:n}))}}),e.exports=r},function(e,t,n){"use strict";function r(e,t){!i.isUndefined(e)&&i.isUndefined(e["Content-Type"])&&(e["Content-Type"]=t)}function o(){var e;return"undefined"!=typeof XMLHttpRequest?e=n(8):"undefined"!=typeof process&&(e=n(8)),e}var i=n(2),s=n(7),u={"Content-Type":"application/x-www-form-urlencoded"},a={adapter:o(),transformRequest:[function(e,t){return s(t,"Content-Type"),i.isFormData(e)||i.isArrayBuffer(e)||i.isBuffer(e)||i.isStream(e)||i.isFile(e)||i.isBlob(e)?e:i.isArrayBufferView(e)?e.buffer:i.isURLSearchParams(e)?(r(t,"application/x-www-form-urlencoded;charset=utf-8"),e.toString()):i.isObject(e)?(r(t,"application/json;charset=utf-8"),JSON.stringify(e)):e}],transformResponse:[function(e){if("string"==typeof e)try{e=JSON.parse(e)}catch(e){}return e}],timeout:0,xsrfCookieName:"XSRF-TOKEN",xsrfHeaderName:"X-XSRF-TOKEN",maxContentLength:-1,validateStatus:function(e){return e>=200&&e<300}};a.headers={common:{Accept:"application/json, text/plain, */*"}},i.forEach(["delete","get","head"],function(e){a.headers[e]={}}),i.forEach(["post","put","patch"],function(e){a.headers[e]=i.merge(u)}),e.exports=a},function(e,t,n){"use strict";var r=n(2);e.exports=function(e,t){r.forEach(e,function(n,r){r!==t&&r.toUpperCase()===t.toUpperCase()&&(e[t]=n,delete e[r])})}},function(e,t,n){"use strict";var r=n(2),o=n(9),i=n(12),s=n(13),u=n(14),a=n(10),c="undefined"!=typeof window&&window.btoa&&window.btoa.bind(window)||n(15);e.exports=function(e){return new Promise(function(t,f){var p=e.data,d=e.headers;r.isFormData(p)&&delete d["Content-Type"];var l=new XMLHttpRequest,h="onreadystatechange",m=!1;if("undefined"==typeof window||!window.XDomainRequest||"withCredentials"in l||u(e.url)||(l=new window.XDomainRequest,h="onload",m=!0,l.onprogress=function(){},l.ontimeout=function(){}),e.auth){var y=e.auth.username||"",w=e.auth.password||"";d.Authorization="Basic "+c(y+":"+w)}if(l.open(e.method.toUpperCase(),i(e.url,e.params,e.paramsSerializer),!0),l.timeout=e.timeout,l[h]=function(){if(l&&(4===l.readyState||m)&&(0!==l.status||l.responseURL&&0===l.responseURL.indexOf("file:"))){var n="getAllResponseHeaders"in l?s(l.getAllResponseHeaders()):null,r=e.responseType&&"text"!==e.responseType?l.response:l.responseText,i={data:r,status:1223===l.status?204:l.status,statusText:1223===l.status?"No Content":l.statusText,headers:n,config:e,request:l};o(t,f,i),l=null}},l.onerror=function(){f(a("Network Error",e,null,l)),l=null},l.ontimeout=function(){f(a("timeout of "+e.timeout+"ms exceeded",e,"ECONNABORTED",l)),l=null},r.isStandardBrowserEnv()){var g=n(16),v=(e.withCredentials||u(e.url))&&e.xsrfCookieName?g.read(e.xsrfCookieName):void 0;v&&(d[e.xsrfHeaderName]=v)}if("setRequestHeader"in l&&r.forEach(d,function(e,t){"undefined"==typeof p&&"content-type"===t.toLowerCase()?delete d[t]:l.setRequestHeader(t,e)}),e.withCredentials&&(l.withCredentials=!0),e.responseType)try{l.responseType=e.responseType}catch(t){if("json"!==e.responseType)throw t}"function"==typeof e.onDownloadProgress&&l.addEventListener("progress",e.onDownloadProgress),"function"==typeof e.onUploadProgress&&l.upload&&l.upload.addEventListener("progress",e.onUploadProgress),e.cancelToken&&e.cancelToken.promise.then(function(e){l&&(l.abort(),f(e),l=null)}),void 0===p&&(p=null),l.send(p)})}},function(e,t,n){"use strict";var r=n(10);e.exports=function(e,t,n){var o=n.config.validateStatus;n.status&&o&&!o(n.status)?t(r("Request failed with status code "+n.status,n.config,null,n.request,n)):e(n)}},function(e,t,n){"use strict";var r=n(11);e.exports=function(e,t,n,o,i){var s=new Error(e);return r(s,t,n,o,i)}},function(e,t){"use strict";e.exports=function(e,t,n,r,o){return e.config=t,n&&(e.code=n),e.request=r,e.response=o,e}},function(e,t,n){"use strict";function r(e){return encodeURIComponent(e).replace(/%40/gi,"@").replace(/%3A/gi,":").replace(/%24/g,"$").replace(/%2C/gi,",").replace(/%20/g,"+").replace(/%5B/gi,"[").replace(/%5D/gi,"]")}var o=n(2);e.exports=function(e,t,n){if(!t)return e;var i;if(n)i=n(t);else if(o.isURLSearchParams(t))i=t.toString();else{var s=[];o.forEach(t,function(e,t){null!==e&&"undefined"!=typeof e&&(o.isArray(e)?t+="[]":e=[e],o.forEach(e,function(e){o.isDate(e)?e=e.toISOString():o.isObject(e)&&(e=JSON.stringify(e)),s.push(r(t)+"="+r(e))}))}),i=s.join("&")}return i&&(e+=(e.indexOf("?")===-1?"?":"&")+i),e}},function(e,t,n){"use strict";var r=n(2),o=["age","authorization","content-length","content-type","etag","expires","from","host","if-modified-since","if-unmodified-since","last-modified","location","max-forwards","proxy-authorization","referer","retry-after","user-agent"];e.exports=function(e){var t,n,i,s={};return e?(r.forEach(e.split("\n"),function(e){if(i=e.indexOf(":"),t=r.trim(e.substr(0,i)).toLowerCase(),n=r.trim(e.substr(i+1)),t){if(s[t]&&o.indexOf(t)>=0)return;"set-cookie"===t?s[t]=(s[t]?s[t]:[]).concat([n]):s[t]=s[t]?s[t]+", "+n:n}}),s):s}},function(e,t,n){"use strict";var r=n(2);e.exports=r.isStandardBrowserEnv()?function(){function e(e){var t=e;return n&&(o.setAttribute("href",t),t=o.href),o.setAttribute("href",t),{href:o.href,protocol:o.protocol?o.protocol.replace(/:$/,""):"",host:o.host,search:o.search?o.search.replace(/^\?/,""):"",hash:o.hash?o.hash.replace(/^#/,""):"",hostname:o.hostname,port:o.port,pathname:"/"===o.pathname.charAt(0)?o.pathname:"/"+o.pathname}}var t,n=/(msie|trident)/i.test(navigator.userAgent),o=document.createElement("a");return t=e(window.location.href),function(n){var o=r.isString(n)?e(n):n;return o.protocol===t.protocol&&o.host===t.host}}():function(){return function(){return!0}}()},function(e,t){"use strict";function n(){this.message="String contains an invalid character"}function r(e){for(var t,r,i=String(e),s="",u=0,a=o;i.charAt(0|u)||(a="=",u%1);s+=a.charAt(63&t>>8-u%1*8)){if(r=i.charCodeAt(u+=.75),r>255)throw new n;t=t<<8|r}return s}var o="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";n.prototype=new Error,n.prototype.code=5,n.prototype.name="InvalidCharacterError",e.exports=r},function(e,t,n){"use strict";var r=n(2);e.exports=r.isStandardBrowserEnv()?function(){return{write:function(e,t,n,o,i,s){var u=[];u.push(e+"="+encodeURIComponent(t)),r.isNumber(n)&&u.push("expires="+new Date(n).toGMTString()),r.isString(o)&&u.push("path="+o),r.isString(i)&&u.push("domain="+i),s===!0&&u.push("secure"),document.cookie=u.join("; ")},read:function(e){var t=document.cookie.match(new RegExp("(^|;\\s*)("+e+")=([^;]*)"));return t?decodeURIComponent(t[3]):null},remove:function(e){this.write(e,"",Date.now()-864e5)}}}():function(){return{write:function(){},read:function(){return null},remove:function(){}}}()},function(e,t,n){"use strict";function r(){this.handlers=[]}var o=n(2);r.prototype.use=function(e,t){return this.handlers.push({fulfilled:e,rejected:t}),this.handlers.length-1},r.prototype.eject=function(e){this.handlers[e]&&(this.handlers[e]=null)},r.prototype.forEach=function(e){o.forEach(this.handlers,function(t){null!==t&&e(t)})},e.exports=r},function(e,t,n){"use strict";function r(e){e.cancelToken&&e.cancelToken.throwIfRequested()}var o=n(2),i=n(19),s=n(20),u=n(6),a=n(21),c=n(22);e.exports=function(e){r(e),e.baseURL&&!a(e.url)&&(e.url=c(e.baseURL,e.url)),e.headers=e.headers||{},e.data=i(e.data,e.headers,e.transformRequest),e.headers=o.merge(e.headers.common||{},e.headers[e.method]||{},e.headers||{}),o.forEach(["delete","get","head","post","put","patch","common"],function(t){delete e.headers[t]});var t=e.adapter||u.adapter;return t(e).then(function(t){return r(e),t.data=i(t.data,t.headers,e.transformResponse),t},function(t){return s(t)||(r(e),t&&t.response&&(t.response.data=i(t.response.data,t.response.headers,e.transformResponse))),Promise.reject(t)})}},function(e,t,n){"use strict";var r=n(2);e.exports=function(e,t,n){return r.forEach(n,function(n){e=n(e,t)}),e}},function(e,t){"use strict";e.exports=function(e){return!(!e||!e.__CANCEL__)}},function(e,t){"use strict";e.exports=function(e){return/^([a-z][a-z\d\+\-\.]*:)?\/\//i.test(e)}},function(e,t){"use strict";e.exports=function(e,t){return t?e.replace(/\/+$/,"")+"/"+t.replace(/^\/+/,""):e}},function(e,t){"use strict";function n(e){this.message=e}n.prototype.toString=function(){return"Cancel"+(this.message?": "+this.message:"")},n.prototype.__CANCEL__=!0,e.exports=n},function(e,t,n){"use strict";function r(e){if("function"!=typeof e)throw new TypeError("executor must be a function.");var t;this.promise=new Promise(function(e){t=e});var n=this;e(function(e){n.reason||(n.reason=new o(e),t(n.reason))})}var o=n(23);r.prototype.throwIfRequested=function(){if(this.reason)throw this.reason},r.source=function(){var e,t=new r(function(t){e=t});return{token:t,cancel:e}},e.exports=r},function(e,t){"use strict";e.exports=function(e){return function(t){return e.apply(null,t)}}}])});
 //# sourceMappingURL=axios.min.map
-
 /**
  * my app code Evenmorevago v1.0
  * Name Pop up js library
@@ -10970,7 +10969,9 @@ var vm = new Vue({
         js_id: '',
         my_reviews: '',
         arrIndex: 0,
-        someInteger: 0
+        someInteger: 0,
+        my_resource_url: '',
+        myIndex: 0
     },
     computed : {
         /**
@@ -10983,6 +10984,7 @@ var vm = new Vue({
         },
         /**
          * this function defines api endpoint dynamically
+         * @returns {string|string}
          */
         myApiUrl: function () {
             if (window.location.host == "localhost" || window.location.host == "127.0.0.1") {
@@ -10994,10 +10996,32 @@ var vm = new Vue({
         },
         /**
          * get the id to call api
+         * @returns {string | null | *}
          */
         myApiId: function () {
             this.js_id = document.querySelector('script[data-name="_emv"][data-token]').getAttribute('data-token');
             return this.js_id;
+        },
+        /**
+         * this function returns the url for resources
+         * @returns {string|string}
+         */
+        myResourceUrl: function () {
+            if (window.location.host == "localhost" || window.location.host == "127.0.0.1") {
+                this.my_resource_url = "http://127.0.0.1:8000";
+            } else {
+                this.my_resource_url = "http://evenmorevago.com";
+            }
+            return this.my_resource_url;
+        },
+        /**
+         * toggles between opacity value
+         * @returns {boolean|*}
+         */
+        toggleOpacity: function () {
+            this.my_opacity = !this.my_opacity;
+            this.my_opacity === true ? 1 : 0;
+            return this.my_opacity;
         }
     },
     methods: {
@@ -11011,7 +11035,7 @@ var vm = new Vue({
             /* getting campaign details for further process*/
             this.getCampaignDetails();
             /* adding custom component for popups */
-            div.innerHTML       = '<my-popups :reviews="my_reviews"></my-popups>';
+            // div.innerHTML       = '<my-popups :reviews="my_reviews"></my-popups>';
         },
         /**
          * this function get the campaign details from api call
@@ -11048,56 +11072,45 @@ var vm = new Vue({
         formPopUps: function (sticky_reviews) {
             if (sticky_reviews.length > 0) {
                 this.my_reviews = sticky_reviews;
-                console.log(this.my_reviews);
-                // var targetDiv = document.getElementById(this.myDivId);
-                // console.log(targetDiv);
-                // var vmm = this;
-                // setInterval(function () {
-                //     console.log(vmm.my_reviews[vmm.arrIndex++].sticky_review_name);
-                //     if (vmm.arrIndex === sticky_reviews.length) {
-                //         vmm.arrIndex = 0;
-                //     }
-                // },1000);
+                var targetDiv = document.getElementById(this.myDivId);
+                var vmm = this;
+                setInterval(function () {
+                    vmm.myIndex = vmm.arrIndex++;
+                    targetDiv.innerHTML = '<div id="sticky-reviews-rectangular-two" class="animated ">' +
+                        '<div class="sticky-reviews-left">' +
+                        '<img src="'+vmm.myResourceUrl+'/uploads/'+vmm.my_reviews[vmm.myIndex].image+'" alt="sticky_review_image">' +
+                        '</div>' +
+                        '<div class="sticky-reviews-right">' +
+                        '<span id="close-morevago-proofs" class="close">X</span>' +
+                        '<h3>' + vmm.my_reviews[vmm.myIndex].sticky_review_name + '</h3>' +
+                        '<h4>' + vmm.my_reviews[vmm.myIndex].memo + '</h4>' +
+                        '<h5>' +
+                        '<img draggable="false" class="emoji" alt="⭐" src="https://s.w.org/images/core/emoji/2.4/svg/2b50.svg">' +
+                        '<img draggable="false" class="emoji" alt="⭐" src="https://s.w.org/images/core/emoji/2.4/svg/2b50.svg">' +
+                        '<img draggable="false" class="emoji" alt="⭐" src="https://s.w.org/images/core/emoji/2.4/svg/2b50.svg">' +
+                        '<img draggable="false" class="emoji" alt="⭐" src="https://s.w.org/images/core/emoji/2.4/svg/2b50.svg">' +
+                        '<img draggable="false" class="emoji" alt="⭐" src="https://s.w.org/images/core/emoji/2.4/svg/2b50.svg">' +
+                        '</h5>' +
+                        '</div>' +
+                        '</div>';
+                    // targetDiv.innerHTML = vmm.my_reviews[vmm.arrIndex++].sticky_review_name;
+                    if (vmm.arrIndex === sticky_reviews.length) {
+                        vmm.arrIndex = 0;
+                    }
+                },1000);
             } else {
                 console.warn('Please assign sticky reviews with your campaign in Evenmorevago dashboard');
             }
+        }
+    },
+    watch: {
+        myIndex: function () {
+            console.log('change');
         }
     }
 });
 /* rendering DOM element */
 vm.createDomElement();
-
-/* component for pop ups */
-Vue.component('my-popups',{
-    props: ['reviews'],
-    template: '<div id="sticky-reviews-rectangular-two" style="animation-name: fade-out-to-bottom;">\n' +
-    '\t\t<div class="sticky-reviews-left">\n' +
-    '\t\t\t<img src="https://morevago-images.s3-accelerate.amazonaws.com/morevago_thumb/images/proof/5a5582c6d7aa4990591435.jpg" alt="">\n' +
-    '\t\t</div>\n' +
-    '\t\t<div class="sticky-reviews-right">\n' +
-    '\t\t\t<span id="close-morevago-proofs" class="close">X</span>\n' +
-    '\t\t\t<h3>Sam Oldham</h3>\n' +
-    '\t\t\t<h4>Crazy how easy Morevago is to implement!</h4>\n' +
-    '\t\t\t<h5>\n' +
-    '\t\t\t\t<img draggable="false" class="emoji" alt="⭐" src="https://s.w.org/images/core/emoji/2.4/svg/2b50.svg">\n' +
-    '\t\t\t\t<img draggable="false" class="emoji" alt="⭐" src="https://s.w.org/images/core/emoji/2.4/svg/2b50.svg">\n' +
-    '\t\t\t\t<img draggable="false" class="emoji" alt="⭐" src="https://s.w.org/images/core/emoji/2.4/svg/2b50.svg">\n' +
-    '\t\t\t\t<img draggable="false" class="emoji" alt="⭐" src="https://s.w.org/images/core/emoji/2.4/svg/2b50.svg">\n' +
-    '\t\t\t\t<img draggable="false" class="emoji" alt="⭐" src="https://s.w.org/images/core/emoji/2.4/svg/2b50.svg">\n' +
-    '\t\t\t</h5>\n' +
-    '\t\t</div>\n' +
-    '\t</div>',
-    mounted : function(){
-
-        this.todo()
-    },
-    methods : {
-
-        todo : function(){
-            console.log('Brasil');
-        }
-    },
-});
 
 /* mounting with the div id */
 vm.$mount('#'+vm.myDivId);
